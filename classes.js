b327efc4527a0025d3bc3d3b5f8479b070b8c3c8
PHOEBE CURTIN <phoebe@PHOEBEs-MacBook-Air.local>
2020-04-24 16:09:54 +1200

    blah

--- classes.js
+++ classes.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
+  two()
+  three()
+  makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -20,6 +23,23 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
-// CREATE FUNCTION three HERE
+function two () {
+  
+  var two = document.getElementById('two')
+  
+  two.classList.add('green')
+}
 
-// CREATE FUNCTION makeVisible HERE
+function three () {
+  var three = document.getElementById('three')
+
+  three.classList.add('purple')
+}
+
+function makeVisible() {
+  var invisible = document.getElementsByClassName('invisible')[0]
+
+four.classList.add('visible')
+}
+
+
